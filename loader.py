ed63ee0af1dc99c7fb7f6b3e28c1056379da3a59
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-03-12 16:18:42 +0300

    Added BASE_DIR and custom handler

--- loader.py
+++ loader.py
@@ -1,1 +1,12 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+log_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(funcName)s - %(message)s')
+my_handler = RotatingFileHandler(os.path.join(BASE_DIR, "parser.log"), mode='a', maxBytes=2 * 1024 * 1024,
+                                 backupCount=1, encoding="utf8", delay=0)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
